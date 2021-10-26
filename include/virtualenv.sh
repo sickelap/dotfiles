6481312fa24a15380b4f5b022e6306025fcace39
Genadijus Paleckis <genadijus.paleckis@gamesys.co.uk>
2021-10-26 09:59:27 +0100

    update virtualenv setup

--- include/virtualenv.sh
+++ include/virtualenv.sh
@@ -1,5 +1,10 @@
- 
-if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
-  export WORKON_HOME=~/virtualenv
-  source /usr/local/bin/virtualenvwrapper.sh
-fi
+while read file; do
+  if [ -f $file ]; then
+    source $file
+    return
+  fi
+done << EOF
+  /usr/local/bin/virtualenvwrapper.sh
+  /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+EOF
+
