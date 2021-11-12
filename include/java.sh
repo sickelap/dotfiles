2e9f4a10df389d41b51513c8780a30e0bca8420b
Genadijus Paleckis <genadijus.paleckis@gamesys.co.uk>
2021-11-12 18:56:51 +0000

    walk known java_home directories

--- include/java.sh
+++ include/java.sh
@@ -1,3 +1,12 @@
-export JAVA_HOME="/usr/local/Cellar/openjdk@11/11.0.10"
-export PATH=$JAVA_HOME/bin:$PATH
+while read file; do
+  if [ -d $location ]; then
+    export JAVA_HOME="$location"
+    export PATH=$JAVA_HOME/bin:$PATH
+    return
+  fi
+done << EOF
+  /usr/lib/jvm/java-11-openjdk-amd64
+  /usr/local/Cellar/openjdk@11/11.0.10
+EOF
+
 export PATH=/usr/local/Cellar/maven@3.5/3.5.4_1/bin:$PATH
