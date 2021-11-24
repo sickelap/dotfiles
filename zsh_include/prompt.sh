45dc32126d4565c48a872771951a2c1e6f78ef17
Genadijus Paleckis <genadijus.paleckis@gamesys.co.uk>
2021-11-24 09:53:07 +0000

    update prompt

--- zsh_include/prompt.sh
+++ zsh_include/prompt.sh
@@ -1,12 +1,14 @@
 autoload -Uz add-zsh-hook vcs_info
-setopt prompt_subst
 add-zsh-hook precmd vcs_info
 
-PROMPT="
-%F{cyan}%~%f${vcs_info_msg_0_}
-$ "
+precmd() {
+  print ""
+  print -P "%F{cyan}%~%f${vcs_info_msg_0_}"
+  PS1="$ "
+}
 
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' unstagedstr '*'
+zstyle ':vcs_info:*' stagedstr '+'
 zstyle ':vcs_info:git:*' formats ' [%F{green}%b%f%F{yellow}%u%c%f]'
 zstyle ':vcs_info:git:*' actionformats ' [%F{green}%b%f|%F{magenta}%a%f%F{yellow}%u%c%f]'
