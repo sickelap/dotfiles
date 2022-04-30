aec53e96453da35119e84b93163a8bb9546a1bed
sickelap <sickelap@pm.me>
2022-04-30 09:16:04 +0100

    condition alias to neovim

--- include/aliases.sh
+++ include/aliases.sh
@@ -3,8 +3,12 @@ alias ls='ls -F'
 alias ll='ls -lF'
 alias showme='ls -lR'
 alias doas='sudo'
-alias vim='nvim'
-alias vi='nvim'
+
+which nvim 2>&1 >/dev/null
+if [ "$?" == "0" ]; then
+  alias vim='nvim'
+  alias vi='nvim'
+fi
 
 which thefuck 2>&1 >/dev/null
 if [[ $? == 0 ]]; then
