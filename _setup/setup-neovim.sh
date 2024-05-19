010497e7d0fe3ffb9e9aa0a2918714321e8ddd3a
sickelap <sickelap@pm.me>
2024-05-19 09:33:39 +0100

    update latest

--- _setup/setup-neovim.sh
+++ _setup/setup-neovim.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
-if [ -d $HOME/bin/nvim-linux64 -a -z "$NVIM_REINSTALL" ]; then
+if [ -d $HOME/bin/nvim-linux64 -a -z "$REINSTALL" ]; then
   echo "Neovim is already installed"
-  echo "To reinstall, run 'export NVIM_REINSTALL=1' and then run this script again"
+  echo "To reinstall, run REINSTALL=1 $0"
 else
   echo "Installing Neovim"
   mkdir -p $HOME/bin
   cd $HOME/bin
   curl -Lso- https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar xz
   ln -s $HOME/bin/nvim-linux64/bin/nvim $HOME/bin/vim
-  rm -rf $HOME/.{local/share,cache,config}/nvim
-  git clone https://github.com/AstroNvim/AstroNvim $HOME/.config/nvim/
-  git clone https://github.com/sickelap/nvim-astro-user $HOME/.config/nvim/lua/user/
+fi
+
+if [ -d $HOME/.local/share/nvim -a -z "$FORCE" ]; then
+  echo "Neovim is already configured"
+  echo "To reinstall, run FORCE=1 $0"
+else
+  rm -rf $HOME/.{local/share,config}/nvim
+  # git clone https://github.com/AstroNvim/AstroNvim $HOME/.config/nvim/
+  # git clone https://github.com/sickelap/nvim-astro-user $HOME/.config/nvim/lua/user/
+  git clone https://github.com/NvChad/NvChad $HOME/.config/nvim -b v2.0 --depth 1
+  git clone git@github.com:sickelap/nvchad-custom-config $HOME/.config/nvim/lua/custom
 fi
--- include/aliases.sh
+++ include/aliases.sh
@@ -4,12 +4,6 @@ alias ll='ls -lF'
 alias showme='ls -lR'
 alias doas='sudo'
 
-which nvim 2>&1 >/dev/null
-if [ "$?" == "0" ]; then
-  alias vim='nvim'
-  alias vi='nvim'
-fi
-
 which thefuck 2>&1 >/dev/null
 if [[ $? == 0 ]]; then
   eval $(thefuck --alias)
