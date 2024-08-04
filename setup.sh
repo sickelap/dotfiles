fbf43504aa0381d07cf9bb909a388fdb5a90557b
sickelap <sickelap@pm.me>
2024-08-04 10:55:43 +0100

    update script permissions

--- setup.sh
+++ setup.sh
@@ -10,7 +10,6 @@ $THIS_DIR/_setup/setup-rust.sh
 
 # other shit
 ln -sf $THIS_DIR/gitconfig $HOME/.gitconfig
-ln -sf $THIS_DIR/gitconfig.user $HOME/.gitconfig.user
 ln -sf $THIS_DIR/profile $HOME/.bashrc
 ln -sf $THIS_DIR/profile $HOME/.profile
 ln -sf $THIS_DIR/editorconfig $HOME/.editorconfig
