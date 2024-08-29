2caba6d8a85bcd7f525011047476592b0c3ddf88
Genadijus Paleckis <genadijus.paleckis@lowcarboncontracts.uk>
2024-08-29 07:55:02 +0100

    add git completion path

--- include/bash-completion.sh
+++ include/bash-completion.sh
@@ -1,9 +1,9 @@
 while read COMPLETION; do
   if [[ -f $COMPLETION ]]; then
     source $COMPLETION
-    return
   fi
-done << EOF
+done <<EOF
   /opt/homebrew/etc/profile.d/bash_completion.sh
+  $HOME/Projects/git/contrib/completion/git-completion.bash
   /usr/share/bash-completion/bash_completion
 EOF
