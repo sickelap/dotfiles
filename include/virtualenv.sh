ca6041efd42a02500428e698bc2eebb38764baa9
Genadijus Paleckis <sickelap@pm.me>
2024-11-01 14:04:57 +0000

    automatically activate virtual env when available

--- include/virtualenv.sh
+++ include/virtualenv.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 while read -r file; do
   if [[ -f $file ]]; then
     source $file
@@ -9,3 +11,26 @@ done <<EOF
   /usr/local/bin/virtualenvwrapper.sh
   /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 EOF
+
+function try_activate_virtualenv() {
+  if [[ -z "$VIRTUAL_ENV" ]]; then
+
+    for dir in .env .venv env venv; do
+      if [[ -d $dir ]]; then
+        source $dir/bin/activate
+      fi
+    done
+  else
+    parentdir="$(dirname "$VIRTUAL_ENV")"
+    if [[ "$PWD"/ != "$parentdir"/* ]]; then
+      deactivate
+    fi
+  fi
+}
+
+function cd() {
+  builtin cd "$@"
+  try_activate_virtualenv
+}
+
+try_activate_virtualenv
