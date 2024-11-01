ec56da3ca0a5173efe0ee94923d4f517fcce6b42
Genadijus Paleckis <sickelap@pm.me>
2024-11-01 14:04:23 +0000

    add path to python 3.11 on macos

--- include/000_extra-path.sh
+++ include/000_extra-path.sh
@@ -3,6 +3,7 @@ while read EXTRA_PATH; do
     export PATH="$PATH:$EXTRA_PATH"
   fi
 done <<EOF
+  /opt/homebrew/opt/python@3.11/libexec/bin
   /opt/homebrew/bin
   /opt/platform-tools
   $HOME/.dotfiles/scripts
