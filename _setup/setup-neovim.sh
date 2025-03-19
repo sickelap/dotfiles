31f0012b5e45e8525372b6f1522197e991f6a6e0
sickelap <sickelap@pm.me>
2025-03-19 10:10:53 +0000

    update nvim setup

--- _setup/setup-neovim.sh
+++ _setup/setup-neovim.sh
@@ -3,23 +3,31 @@
 OS=$(uname -s)
 
 if [[ $OS == "Linux" ]]; then
-  if [ -d $HOME/bin/nvim-linux64 -a -z "$REINSTALL" ]; then
+  ARCH=$(uname -m)
+  case $ARCH in
+  "x86_64" | "aarch64") ;;
+  *)
+    echo "unknown arch $ARCH"
+    return
+    ;;
+  esac
+  if [ -d "$HOME/bin/nvim-linux-$VARIANT" ] && [ -z "$REINSTALL" ]; then
     echo "Neovim is already installed"
     echo "To reinstall, run REINSTALL=1 $0"
   else
     echo "Installing Neovim"
-    mkdir -p $HOME/bin
-    cd $HOME/bin
-    curl -Lso- https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar xz
-    ln -sf $HOME/bin/nvim-linux64/bin/nvim $HOME/bin/vim
+    mkdir -p "$HOME/bin"
+    cd "$HOME/bin" || true
+    curl -Lso- "https://github.com/neovim/neovim/releases/latest/download/nvim-linux-$ARCH.tar.gz" | tar xz
+    ln -sf "$HOME/bin/nvim-linux-$ARCH/bin/nvim" "$HOME/bin/vim"
   fi
 fi
 
-if [ -d $HOME/.local/share/nvim -a -z "$FORCE" ]; then
+if [ -d "$HOME/.local/share/nvim" ] && [ -z "$FORCE" ]; then
   echo "Neovim is already configured"
   echo "To reinstall, run FORCE=1 $0"
 else
-  rm -rf $HOME/.{local/share,config}/nvim
-  git clone https://github.com/NvChad/NvChad $HOME/.config/nvim -b v2.0 --depth 1
-  git clone https://github.com/sickelap/nvchad-custom-config $HOME/.config/nvim/lua/custom
+  rm -rf "$HOME/.{local/share,config}/nvim"
+  git clone https://github.com/NvChad/NvChad "$HOME/.config/nvim" -b v2.0 --depth 1
+  git clone https://github.com/sickelap/nvchad-custom-config "$HOME/.config/nvim/lua/custom"
 fi
