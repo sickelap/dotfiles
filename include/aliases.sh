a55d6abf0d8cc6625656b98b4045ef50f3acd8d4
sickelap <sickelap@pm.me>
2026-03-26 12:35:19 +0000

    add coregz function

--- include/aliases.sh
+++ include/aliases.sh
@@ -9,3 +9,12 @@ if [ "$?" == "0" ]; then
 fi
 
 alias dotfiles='vim ~/.dotfiles && source ~/.profile'
+
+coregz() {
+  set -e
+  echo -n "creating snapshot ... "
+  rm -rf $HOME/Downloads/repo*
+  git clone file://$HOME/Projects/work/Gousto2-Core $HOME/Downloads/repo --depth 1 2>/dev/null
+  tar czf $HOME/Downloads/repo.tar.gz $HOME/Downloads/repo 2>/dev/null
+  echo "done ($HOME/Downloads/repo.tar.gz)"
+}
